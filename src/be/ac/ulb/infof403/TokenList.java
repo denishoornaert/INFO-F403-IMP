36ac9688ea74a2dbd5efc0e41dbd58443c3bc159
denishoornaert <dhoornae@ulb.ac.be>
2017-10-04 20:29:23 +0200

    Addition of the methods required (toString and add).

--- src/be/ac/ulb/infof403/TokenList.java
+++ src/be/ac/ulb/infof403/TokenList.java
@@ -1,5 +1,26 @@
 package be.ac.ulb.infof403;
 
+import java.util.ArrayList;
+
 public class TokenList {
+    
+    private ArrayList<Symbol> _list;
+    
+    public TokenList() {
+        _list = new ArrayList<>();
+    }
+    
+    public void add(Symbol symbol) {
+        _list.add(symbol);
+    }
+    
+    @Override
+    public String toString() {
+        String res = "";
+        for (Symbol symbol : _list) {
+            res += symbol.toString()+"\n"; // concatenation of \n here cause I don't know if I can modify the given Symbol class
+        }
+        return res;
+    }
     
 }
