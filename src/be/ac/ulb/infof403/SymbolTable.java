37dffaa7c000d5910f893ff6b696a4e429dbccc9
denishoornaert <dhoornae@ulb.ac.be>
2017-10-04 20:56:45 +0200

    completion of the class by the addition of two methods (print and put).

--- src/be/ac/ulb/infof403/SymbolTable.java
+++ src/be/ac/ulb/infof403/SymbolTable.java
@@ -1,5 +1,24 @@
 package be.ac.ulb.infof403;
 
-public class SymbolTable {
+import java.util.HashMap;
+
+public class SymbolTable extends HashMap<Object, Integer> {
+    
+    public SymbolTable() {
+        super();
+    }
+    
+    public void put(Symbol symbol) {
+        this.put(symbol.getValue(), symbol.getLine());
+    }
+    
+    @Override
+    public String toString() {
+    String res = "";
+        for (Entry<Object, Integer> entry : this.entrySet()) {
+            res += entry.getKey()+" "+entry.getValue()+"\n";
+        }
+    return res;
+    }
     
 }
