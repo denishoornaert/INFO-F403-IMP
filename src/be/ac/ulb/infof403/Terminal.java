70cc8974a93fb8e480fcb804683615df99d7442b
denishoornaert <dhoornae@ulb.ac.be>
2017-11-12 15:57:28 +0100

    Introduction of a new abstraction level (useful for the first function) and of a new terminal epsilon.

---
+++ src/be/ac/ulb/infof403/Terminal.java
@@ -0,0 +1,12 @@
+package be.ac.ulb.infof403;
+
+/**
+ * Exists only to structure the code.
+ * In fact, Elem can be either a Variable, a Symbol or an Epsilon but there is a
+ * clear distinction between the Variables and the two other elements.
+ * The later is a Terminal
+ * 
+ */
+public abstract class Terminal extends Elem {
+    
+}
