98966097479e7c05d23f21ab5312e81828728e21
Detobel36 <detobel36@hotmail.fr>
2017-11-25 15:08:16 +0100

    Fix test to work with previous refactoring

--- srcTest/test/be/ac/ulb/infof403/grammar/TestTwiceBegin.java
+++ srcTest/test/be/ac/ulb/infof403/grammar/TestTwiceBegin.java
@@ -2,7 +2,8 @@ package test.be.ac.ulb.infof403.grammar;
 
 import be.ac.ulb.infof403.TokenList;
 import be.ac.ulb.infof403.grammar.Grammar;
-import be.ac.ulb.infof403.parser.AbstractLl1;
+import be.ac.ulb.infof403.parser.StackLl1;
+import be.ac.ulb.infof403.parser.TreeLl1;
 import be.ac.ulb.infof403.parser.UnexpectedCharacterException;
 import be.ac.ulb.infof403.scanner.ImpScanner;
 import be.ac.ulb.infof403.scanner.ImpSyntaxException;
@@ -17,16 +18,29 @@ import org.junit.rules.ExpectedException;
  */
 public class TestTwiceBegin {
     
+    private static final String GRAMMAR_PATH = "./test/grammar/UnambiguousIMP.gram";
+    private static final String IMP_PATH = "./test/imp/TwiceBegin.imp";
+    
     @Rule
     public ExpectedException expected = ExpectedException.none();
     
+    
     @Test
-    public void testTwiceBegin() throws UnexpectedCharacterException, IOException, ImpSyntaxException {
-        final Grammar grammar = Grammar.openAndScanGrammar("./test/grammar/UnambiguousIMP.gram");
-        final TokenList tokenList = new ImpScanner("./test/imp/TwiceBegin.imp", false).getTokenList();
-        final AbstractLl1 ll1 = new AbstractLl1(grammar, tokenList);
+    public void testTwiceBeginStackParsing() throws UnexpectedCharacterException, IOException, ImpSyntaxException {
+        final Grammar grammar = Grammar.openAndScanGrammar(GRAMMAR_PATH);
+        final TokenList tokenList = new ImpScanner(IMP_PATH, false).getTokenList();
+        final StackLl1 ll1 = new StackLl1(grammar, tokenList);
         expected.expect(be.ac.ulb.infof403.parser.UnexpectedCharacterException.class);
-        ll1.stackParse(false);
+        ll1.parse(false);
+    }
+    
+    @Test
+    public void testTwiceBeginTreeParsing() throws UnexpectedCharacterException, IOException, ImpSyntaxException {
+        final Grammar grammar = Grammar.openAndScanGrammar(GRAMMAR_PATH);
+        final TokenList tokenList = new ImpScanner(IMP_PATH, false).getTokenList();
+        final TreeLl1 ll1 = new TreeLl1(grammar, tokenList);
+        expected.expect(be.ac.ulb.infof403.parser.UnexpectedCharacterException.class);
+        ll1.parse(false);
     }
     
     @After
