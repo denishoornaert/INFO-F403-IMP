2bab26f84c63c4fd5bc969ef1acb1ff1079807de
denishoornaert <dhoornae@ulb.ac.be>
2017-12-20 17:36:42 +0100

    complete erad

--- src/be/ac/ulb/infof403/impVariableTypes/Read.java
+++ src/be/ac/ulb/infof403/impVariableTypes/Read.java
@@ -9,5 +9,12 @@ public class Read extends RuleTree {
         super(value);
     }
 
+    @Override
+    public String getRepresentation() {
+        String targetVariable = this._children.get(2).getLocalVariable();
+        String result = targetVariable+" = call void @readInt()";
+        return result;
+    }
+    
 }
 
