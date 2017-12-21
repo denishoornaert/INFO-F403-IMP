7f12d60bbe6abc0063cc53a6cc4b77facc07083d
denishoornaert <dhoornae@ulb.ac.be>
2017-12-21 15:37:16 +0100

    refactor : creation of a common utils class

---
+++ src/Utils/FileController.java
@@ -0,0 +1,50 @@
+package Utils;
+
+import be.ac.ulb.infof403.view.GenerateGojsParseTree;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hoornaert
+ */
+public abstract class FileController {
+    
+    public String readFile(final String fileName) {
+        String result = "";
+        try {
+            final FileReader fileReader = new FileReader(fileName);
+            final BufferedReader bufferedReader = new BufferedReader(fileReader);
+            
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+                result += line + "\n";
+            }   
+            bufferedReader.close();
+        } catch(IOException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");                
+        }
+        
+        return result;
+    }
+    
+    public void writeFile(final String fileOutput, final String content) {
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(fileOutput, "UTF-8");
+            writer.print(content);
+        } catch (FileNotFoundException | UnsupportedEncodingException ex) {
+            Logger.getLogger(GenerateGojsParseTree.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if(writer != null) {
+            writer.close();
+        }
+    }
+    
+}
